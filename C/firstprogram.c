b0761bb032cc53e327ed830a2e632389b1085682
shahadat-mso <sahadat.mso@gmail.com>
2022-10-16 22:58:34 +0600

    mod

--- C/firstprogram.c
+++ C/firstprogram.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 int main()
 {
+    int a=10, b=20, c=30;
+    c=b;
+    printf("%d0", a*c);
+
     return 0;
 }
+
+//task1 area of a triangle; given lenght & height
+//task2 area of a triangle; given lenght of 3 line
