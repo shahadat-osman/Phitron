eea30d6cc5af3ebcaa38851665c18870b5318567
shahadat-mso <sahadat.mso@gmail.com>
2022-10-26 23:21:51 +0600

    mod

--- C/00.c
+++ C/00.c
@@ -1,22 +1,6 @@
 #include<stdio.h>
 int main()
 {
-    int a,b, opt;
-
-    printf("Enter Two numbers: ");
-    scanf("%d %d", &a, &b);
-
-    printf("Enter Option:\n'1' for Sum\n'2' for Sub\n'3' for Multi\n'4' for Div\n");
-    scanf("%d", &opt);
-
-    if(opt==1)
-        printf("Result= %d", a+b);
-    if(opt==2)
-        printf("Result= %d", a-b);
-    if(opt==3)
-        printf("Result= %d", a*b);
-    if(opt==4)
-        printf("Result= %d", a/b);
-
+    
     return 0;
 }
