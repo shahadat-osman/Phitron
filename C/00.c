5f9dcf883f4aebce1a1634177f9a774b31e41f2f
shahadat-mso <sahadat.mso@gmail.com>
2022-10-27 23:32:52 +0600

    mod

--- C/00.c
+++ C/00.c
@@ -1,24 +1,5 @@
 #include<stdio.h>
 int main()
 {
-    int n, m, i, temp=24;
-    scanf("%d %d", &n ,&m);
-    if(n>m)
-    {
-        for(i=n; i<=temp; i++)
-        {
-            
-            if(i==24)
-            {
-                i=0;
-                temp=m;
-            }
-            printf("%d ", i);
-        }
-    }
-    for(i=n; i<=m; i++)
-    {
-        printf("%d ", i);
-    }
     return 0;
 }
