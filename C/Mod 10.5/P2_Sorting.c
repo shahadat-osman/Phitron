090254893ba3ea01feb49d951427e38691abbfeb
shahadat-mso <sahadat.mso@gmail.com>
2022-11-13 00:01:48 +0600

    init

--- C/Mod 10.5/P2_Sorting.c
+++ C/Mod 10.5/P2_Sorting.c
@@ -3,15 +3,33 @@ int main()
 {
     int n, i;
     scanf("%d", &n);
-    int ary[n], sort[n];
+    int ary[n];
     for(i=1; i<=n; i++)
     {
         scanf("%d", &ary[i]);
-        sort[i]=ary[i];
     }
+    //calculate max number of ary[]
+    int max=ary[1];
     for(i=1; i<=n; i++)
     {
-        
+        if(ary[i]>max)
+            max=ary[i];
     }
-
+    //create new array[max+1] and assign 0 in all index of that array.
+    int sort[max];
+    for(i=1; i<=max; i++)
+    {
+        sort[i]=0;
+    }
+    //assign 1 in sort[] index where ary[] values placed.
+    for(i=1; i<=n; i++)
+    {
+        sort[ary[i]]+=1;
+    }
+    //print the position of sort[] and this is the ascending order of ary[] values.
+    for(i=1; i<=max; i++)
+    {
+        if(sort[i]==1)
+            printf("%d ", i);
+    }
 }
