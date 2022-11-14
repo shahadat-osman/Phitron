f7b03d759d62a39a9789e2f7943052feeb66b1da
shahadat-mso <sahadat.mso@gmail.com>
2022-11-14 19:38:33 +0600

    init

---
+++ C/Mod 10.5/P4_find-element.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,max, k, element, count=0;
+    scanf("%d", &n);
+    int ary[n];
+    for(i=1; i<=n; i++)
+    {
+        scanf("%d", &ary[i]);
+    }
+
+    scanf("%d", &k);
+
+    max=ary[1];
+    for(i=1; i<=n; i++)
+    {
+        if(ary[i]>max)
+            max=ary[i];
+    }
+    int alist[max];
+    for(i=1; i<=max; i++)
+    {
+        alist[i]=0;
+    }
+    for(i=1; i<=n; i++)
+    {
+        alist[ary[i]]=ary[i];
+    }
+    for(i=1; i<=max; i++)
+    {   
+        if(count==k)
+            break;
+        if(alist[i]!=0)
+            count++;
+        element=i;
+    }
+    printf("%d\n", element);
+
+}
