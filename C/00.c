b8c3a30f674197c55e6eb666fb01e7e6c233ca1c
shahadat-mso <sahadat.mso@gmail.com>
2023-03-08 23:42:45 +0600

    init

--- C/00.c
+++ C/00.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <string.h>
-
+#include<stdio.h>
+void fun(int n)
+{
+    if(n>0)
+    {
+        fun(n-1);
+        printf("%d ",n);
+    }
+}
 int main()
 {
-    
+    int x=4;
+    fun(x);
     return 0;
 }
--- C/Mod 25.5/P2.c
+++ C/Mod 25.5/P2.c
@@ -1,1 +1,43 @@
 #include<stdio.h>
+void sort(int ary[], int size)
+{
+    int temp, i, j;
+
+    for(i=0; i<size-1; i++)
+    {
+        for(j=0; j<size-1; j++)
+        {
+            if(ary[j]>ary[j+1])
+            {
+                temp=ary[j];
+                ary[j]=ary[j+1];
+                ary[j+1]=temp;
+            }
+        }
+    }
+}
+int smallest(int ary[], int index)
+{
+    return ary[index-1];
+}
+int largest(int ary[], int index, int size)
+{
+    return ary[size-index];
+}
+
+int main()
+{
+    int n, i, k;
+    scanf("%d", &n);
+
+    int ary[n];
+    for(i=0; i<n; i++)
+        scanf("%d", &ary[i]);
+    scanf("%d", &k);
+
+    sort(ary, n);
+    
+    int Kth_smallest=smallest(ary, k);
+    int Kth_largest= largest(ary, k, n);
+    printf("%d smallest- %d\n%d largest- %d\n", k, Kth_smallest, k, Kth_largest);
+}
