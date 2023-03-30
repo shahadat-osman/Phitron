c9e653ce676833048476f18eff4177f951ac1941
shahadat-mso <sahadat.mso@gmail.com>
2023-03-30 17:38:22 +0600

    init

---
+++ DS/W02/Mod5/dynamic_array.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Array // Memory complexity O(n)
+{
+private:
+    int *arr;
+    int cap;
+    int sz;
+
+    void Increase_size()
+    {
+        cap = cap * 2;
+        int *tmp = new int[cap];
+        for (int i = 0; i < sz; i++)
+        {
+            tmp[i] = arr[i];
+        }
+        delete[] arr;
+        arr = tmp;
+    }
+
+public:
+    Array()
+    {
+        arr = new int[1];
+        cap = 1;
+        sz = 0;
+    }
+
+    void Push_back(int x) // Time complexity O(1)
+    {
+        if (cap == sz)
+        {
+            Increase_size();
+        }
+        arr[sz] = x;
+        sz++;
+    }
+    void Insert(int pos, int x) // Time complexity O(sz-pos) O(sz)
+    {
+        if (cap == sz)
+        {
+            Increase_size();
+        }
+        for (int i = sz - 1; i >= pos; i--)
+        {
+            arr[i + 1] = arr[i];
+        }
+        arr[pos] = x;
+        sz++;
+    }
+    void Print() // Time complexity O(n)
+    {
+        for (int i = 0; i < sz; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    int getSize() // Time complexity O(1)
+    {
+        return sz;
+    }
+    int getElement(int idx) // Time complexity O(1)
+    {
+        if (idx >= sz)
+        {
+            cout << "Error " << idx << " is out of Bound!\n";
+            return -1;
+        }
+        return arr[idx];
+    }
+};
+
+int main()
+{
+    Array a;
+    a.Push_back(10);
+    a.Push_back(20);
+    a.Push_back(30);
+    a.Push_back(40);
+    a.Push_back(50);
+
+    cout << a.getSize() << "\n";
+    a.Print();
+    
+    a.Insert(1, 5);
+
+    cout << a.getSize() << "\n";
+    for (int i = 0; i < a.getSize(); i++)
+    {
+        cout << a.getElement(i) << " ";
+    }
+    return 0;
+}
