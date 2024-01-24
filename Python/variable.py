afbfb0e6fa6a4eabb5367a45b4943b0bd8833da2
shahadat-osman <sahadat.mso@gmail.com>
2024-01-24 21:14:56 +0600

    new file

---
+++ Python/variable.py
@@ -0,0 +1,8 @@
+# print("Give me some money: ")
+# input('Give me some money: ')
+
+first_money = input('Received money from first person: ')
+second_money = input("Received money from second person: ")
+
+print("Here is your money ", first_money)
+print("Here is your money ", second_money) 
